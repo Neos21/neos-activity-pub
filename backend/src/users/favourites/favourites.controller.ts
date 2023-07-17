29e40857642fcfdcfd6c3420d7ee509977d32c85
Neos21 <neos21@gmail.com>
2023-07-17 16:50:57 +0900

    [WIP] Insert が止まる

--- backend/src/users/favourites/favourites.controller.ts
+++ backend/src/users/favourites/favourites.controller.ts
@@ -11,21 +11,16 @@ export class FavouritesController {
   @UseGuards(JwtAuthGuard)
   @Post(':name/favourites')
   public async create(@Param('name') name: string, @Body('userName') userName: string, @Body('postId') postId: string, @Body('userId') userId: string, @Req() req: Request, @Res() res: Response): Promise<Response> {
-    console.log('POST');
     const jwtUserName = (req.user as { name: string; })?.name;
     if(jwtUserName == null) return res.status(HttpStatus.BAD_REQUEST).json({ error: 'JWT User Name Is Empty' });
     if(name !== userName || name !== jwtUserName || userName !== jwtUserName) return res.status(HttpStatus.BAD_REQUEST).json({ error: 'Invalid User Name' });
     
     try {
-      console.log('Start 1')
       const inboxUrl = await this.favouritesService.fetchInboxUrl(userId);
-      console.log('Start 2', inboxUrl)
       await this.favouritesService.postLikeInbox(userName, inboxUrl, postId);
-      console.log('Start 3');
-      await this.favouritesService.create(userName, postId, inboxUrl);
+      this.favouritesService.create(userName, postId, inboxUrl);
     }
     catch(error) {
-      console.log('ERROR', error);
       return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({ error });
     }
   }
